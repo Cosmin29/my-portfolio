ce74820cd7844fce7bd01e6c61867a15e099291c
Cosmin29 <73159321+Cosmin29@users.noreply.github.com>
2022-05-03 15:23:09 +0300

    Added services and portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/Login Page.png'
+import IMG2 from '../../assets/Home page.png'
+import IMG3 from '../../assets/Profile page.png'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'WeebSocial Login Page',
+    github: 'https://github.com/Cosmin29/G11-WebAppProgramming-C.M.O./tree/main1.1',
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'WeebSocial Home Page',
+    github: 'https://github.com/Cosmin29/G11-WebAppProgramming-C.M.O./tree/main1.1',
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'WeebSocial Profile Page',
+    github: 'https://github.com/Cosmin29/G11-WebAppProgramming-C.M.O./tree/main1.1',
+    demo: ''
+  },
+
+]
+
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio_container">
+        {
+          data.map(({id, image, title, github}) => {
+            return (
+              <article key={id} className='portfolio_item'>
+              <div className="portfolio_item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio_item-cta">
+                <a href={github} className="btn">Github</a>
+              </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,89 @@
 import React from 'react'
 import './services.css'
+import {AiOutlineCheckCircle} from 'react-icons/ai'
 const Services = () => {
   return (
     <section id='services'>
-    
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className ="container services_containers">
+        <article className="services">
+          <div className ="service_head">
+            <h3>UI Design</h3>
+          </div>
+
+          <ul className ="service_list">
+            <li>
+              <AiOutlineCheckCircle className ="service_list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+            </li>
+            <li>
+              <AiOutlineCheckCircle className ="service_list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+            </li>
+            <li>
+              <AiOutlineCheckCircle className ="service_list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+            </li>
+            <li>
+              <AiOutlineCheckCircle className ="service_list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF UI */}
+        <article className="services">
+          <div className ="service_head">
+            <h3>WEB DEVELOPMENT</h3>
+          </div>
+
+          <ul className ="service_list">
+            <li>
+              <AiOutlineCheckCircle className ="service_list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+            </li>
+            <li>
+              <AiOutlineCheckCircle className ="service_list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+            </li>
+            <li>
+              <AiOutlineCheckCircle className ="service_list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+            </li>
+            <li>
+              <AiOutlineCheckCircle className ="service_list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF WEB DEVELOPMENT */}
+        <article className="services">
+          <div className ="service_head">
+            <h3>HOBBIES</h3>
+          </div>
+
+          <ul className ="service_list">
+            <li>
+              <AiOutlineCheckCircle className ="service_list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+            </li>
+            <li>
+              <AiOutlineCheckCircle className ="service_list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+            </li>
+            <li>
+              <AiOutlineCheckCircle className ="service_list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+            </li>
+            <li>
+              <AiOutlineCheckCircle className ="service_list-icon"/>
+              <p>Lorem ipsum dolor sit amet, consectetur adip</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF HOBBIES */}
+      </div>
     </section>
   )
 }
